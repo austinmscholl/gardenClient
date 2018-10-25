71f59e9476d6a1685b94d6a9b47f1d299e17e398
Drew Blincoe <Drew@Drews-MacBook-Pro.local>
2018-10-25 12:45:22 -0400

    oh yeah

--- src/app/delete.service.ts
+++ src/app/delete.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-import { ProductsService } from './products.service';
+import { HttpHeaders } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeleteService {
 
-  constructor(private http: HttpClient, private productsService: ProductsService) { }
+  constructor(private http: HttpClient) { }
+
+  
 
   deleteProduct(id) {
-    return this.http.delete(`https://efa-gardenapp-backend.herokuapp.com/api/product/${id}`)
+  let headers = {
+    headers: new HttpHeaders({
+      'Authorization': JSON.parse(localStorage.getItem('token'))
+    })
+  }
+    return this.http.delete(`https://efa-gardenapp-backend.herokuapp.com/api/product/${id}`, headers)
       .subscribe(res => console.log(res))
-    // this.http.get('https://efa-gardenapp-backend.herokuapp.com/api/product')
-
-    // console.log(id)
   }
 }
